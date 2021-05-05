841ac688a5f1e846188b293ef866a827181fe5d3
Joao Vitor Pinto Andrade <joaodbv@outlook.com>
2021-05-05 14:31:25 -0300

    Primeiro Commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria Uchoa',
+    email: 'maria@josefa.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Carlos Henrique',
+    email: 'carlosbolota@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'João Vitor P. Andrade',
+    email: 'Umemail@qualquer',
+    avatarUrl: 'https://avatarfiles.alphacoders.com/916/91685.jpg',
+  ),
+};
